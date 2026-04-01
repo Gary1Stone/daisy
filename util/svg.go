34b7da98f1ad59899e40473fafd87fe668cb2b70
gary1stone <gary.t.stone@gmail.com>
2026-04-01 17:28:36 -0400

    More performant iconmap

--- util/svg.go
+++ util/svg.go
@@ -7,10 +7,14 @@ import (
 	"strings"
 )
 
-var iconMap map[string][]byte
+var iconMap map[string]string
+
+const defaultIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-unknown"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2" /><path d="M12 17v.01" /><path d="M12 14a1.5 1.5 0 1 0 -1.14 -2.474" /></svg>`
 
 // Read all the icon svg files into a MAP for fast access
 func init() {
+	iconMap = make(map[string]string)
+
 	// Get the current working directory
 	wd, err := os.Getwd()
 	if err != nil {
@@ -21,41 +25,34 @@ func init() {
 	// Get the path to the icon directory
 	iconDir := filepath.Join(wd, "web", "public", "icons")
 
-	// Read all the svg files into a map from that directory with the filname as the key
+	// Read all the svg files into a map from that directory with the filename as the key
 	files, err := os.ReadDir(iconDir)
 	if err != nil {
-		log.Println(err)
+		log.Println("Error reading icon directory:", err)
 		return
 	}
 
 	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
 		// Skip non-svg files
-		if strings.ToLower(filepath.Ext(file.Name())) != ".svg" {
+		if file.IsDir() || strings.ToLower(filepath.Ext(file.Name())) != ".svg" {
 			continue
 		}
 
-		if iconMap == nil {
-			iconMap = make(map[string][]byte)
-		}
 		// Read the file
 		filename := file.Name()
 		data, err := os.ReadFile(filepath.Join(iconDir, filename))
 		if err != nil {
-			log.Println(err)
+			log.Println("Error reading icon file:", filename, err)
 			continue
 		}
-		iconMap[filename] = data
+		iconMap[filename] = string(data)
 	}
 }
 
-// Read in an svg file from the ./web/icons directory
+// GetIcon returns the svg content from the map or a default icon if not found
 func GetIcon(filename string) string {
-	defaultSvg := `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-unknown"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2" /><path d="M12 17v.01" /><path d="M12 14a1.5 1.5 0 1 0 -1.14 -2.474" /></svg>`
-	if filename == "" || iconMap == nil || len(iconMap[filename]) == 0 {
-		return defaultSvg
+	if icon, ok := iconMap[filename]; ok {
+		return icon
 	}
-	return string(iconMap[filename])
+	return defaultIcon
 }
