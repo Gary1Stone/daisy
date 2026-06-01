f33e84ebbe926602ced031964773f8695ff0676d
gary1stone <gary.t.stone@gmail.com>
2026-06-01 13:20:50 -0400

    Update ticlet for pico.css

--- cmd/Ticket.go
+++ cmd/Ticket.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gbsto/daisy/ctrls"
 	"github.com/gbsto/daisy/db"
+	"github.com/gbsto/daisy/svg"
 	"github.com/gbsto/daisy/util"
 
 	"github.com/gofiber/fiber/v2"
@@ -84,8 +85,8 @@ func GetTicket(c *fiber.Ctx) error {
 		action.Gid = db.FindGroupID(user.Uid, action.Gid, true)
 	}
 
-	return c.Render("ticket", fiber.Map{
-		"title":         template.HTML("<span class='mif-news icon'></span>&nbsp;Ticket"),
+	return c.Render("ticket", addNavigationIcons(fiber.Map{
+		"title":         template.HTML(svg.GetIcon("news") + " Ticket"),
 		"fullName":      user.Fullname,
 		"cmd_one":       template.HTML(ctrls.BuildRouteButton(user.Permissions.Ticket.Update)),
 		"isAdmin":       user.IsAdmin,
@@ -119,7 +120,7 @@ func GetTicket(c *fiber.Ctx) error {
 		"assignGroup":   action.AssignedGroupName,
 		"assignUser":    action.AssignedUserName,
 		"informList":    template.HTML(ctrls.BuildInformList(aid)),
-	})
+	}))
 }
 
 // Removes the specified prefix and suffix from the input string,
@@ -131,4 +132,59 @@ func removePrefixSuffixAnchor(prefix, suffix, input string) string {
 	}
 	re := regexp.MustCompile(`<a[^>]*>(.*?)</a>`)
 	return re.ReplaceAllString(input, "$1")
+}
+
+func PostTicket(c *fiber.Ctx) error {
+	response := "Critical Server Error!"
+
+	user, err := extractUserInfo(c)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).Redirect("index.html")
+	}
+
+	recvd := new(db.Ticket)
+	if err := c.BodyParser(recvd); err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusOK).SendString(response)
+	}
+
+	switch recvd.Task {
+	case "get_person_control":
+		// If there is a group, and no user, but a device, lookup that device's assigned user (if any)
+		if recvd.Gid > 0 && recvd.Uid == 0 && recvd.Cid > 0 {
+			recvd.Uid, _ = db.GetDeviceAssignedUserGroup(user.Uid, recvd.Cid)
+		}
+		response = ctrls.BuildDropList("USER", strconv.Itoa(recvd.Uid), strconv.Itoa(recvd.Gid), true, false)
+	case "get_inform_person_control":
+		// If there is a group, and no user, but a device, lookup that device's assigned user (if any)
+		if recvd.Gid > 0 && recvd.Uid == 0 && recvd.Cid > 0 {
+			recvd.Uid, _ = db.GetDeviceAssignedUserGroup(user.Uid, recvd.Cid)
+		}
+		response = ctrls.BuildDropList("USERINFORM", strconv.Itoa(recvd.Uid), strconv.Itoa(recvd.Inform_gid), true, false)
+	case "route_ticket":
+		recvd.Cmd = "ROUTE"
+		if recvd.OldGid != recvd.Gid || recvd.OldUid != recvd.Uid {
+			recvd.Log += " Reassigned from: " + recvd.OldGroup + ", " + recvd.OldUser
+			recvd.Log = strings.TrimSpace(recvd.Log)
+		}
+		err := db.SetTicket(user.Uid, recvd)
+		if err != nil {
+			log.Println(err)
+		}
+		response = ctrls.BuildWorklog(user.Uid, recvd.Aid)
+	case "add_log":
+		err := db.AddLog(user.Uid, recvd)
+		if err != nil {
+			log.Println(err)
+		}
+		if recvd.Cmd == "CLOSED" {
+			db.AckAction(user.Uid, recvd.Aid, true, true, true)
+			db.AckAlert(user.Uid, user.Uid, recvd.Aid, true, true, false)
+			// TODO: Close ticket or mark as closed and notify user ???????????????????
+			// Concept: have a notify at close flag in Alerts table DONE
+
+		}
+		response = ctrls.BuildWorklog(user.Uid, recvd.Aid)
+	}
+	return c.Status(fiber.StatusOK).SendString(response)
 }
